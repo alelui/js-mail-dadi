c45ff539a2602d52d10844e20a817da3c7fd9951
Alessandro <luisialessandro95@gmail.com>
2021-11-10 23:25:31 +0100

    migliorie finalissime

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
-// Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 const mail = prompt("Inserisci dal punto di vista stilistico dell'emai");
 const mail_List = ['mickey@disney.com', 'donald@disney.com', 'goofy@disney.com', 'daisy@disney.com', 'unclescrooge@disney.com'];
@@ -6,6 +6,7 @@ const mail_List = ['mickey@disney.com', 'donald@disney.com', 'goofy@disney.com',
 let sentinel = false;
 let i;
 let n;
+
 for ( i= 0; i < mail_List.length; i++){
 
     if ( mail == mail_List[i]){
@@ -17,13 +18,17 @@ for ( i= 0; i < mail_List.length; i++){
 const box = document.getElementById('mailBox');
 
 if (sentinel){
-    box.innerHTML = "Your e-mail is: " + mail_List[n];
+    box.innerHTML = "Your are in the list! " + mail_List[n];
 }else{
-    box.innerHTML = 'Sorry, we don\'t konw "' + mail + '"';
+    box.innerHTML = 'Sorry, we don\'t konw you! "' + mail + '"';
 }    
 
 
-// Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // userNum = parseInt(prompt('Insert a number'));
 userNum = Math.floor(Math.random() * 6) + 1;
@@ -39,11 +44,11 @@ machine.innerHTML = 'Machine ' + randomNum;
 
 
 if ( userNum > randomNum ){
-    luckyBox.innerHTML = 'User Wins'
+    luckyBox.innerHTML = 'User Wins';
 }else{
     if ( userNum < randomNum ){
-        luckyBox.innerHTML = 'Machine Wins'
+        luckyBox.innerHTML = 'Machine Wins';
     }else{
-        luckyBox.innerHTML = 'Even Game'
+        luckyBox.innerHTML = 'Even Game';
     }
 }
